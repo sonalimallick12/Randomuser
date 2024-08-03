dd1b01d86c85499fe44a641515662a7399eaf74b
sonali mallick <mallicksonali624@gmail.com>
2024-08-04 04:36:46 +0530

    ADD : 3 MODIFIED

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(data => {
             const user = data.results[0];
             document.getElementById('profile-picture').src = user.picture.large;
-            document.getElementById('name').textContent = `${user.name.first} ${user.name.last}`;
-            document.getElementById('email').textContent = user.email;
-            document.getElementById('dob').textContent = new Date(user.dob.date).toLocaleDateString();
-            document.getElementById('location').textContent = `${user.location.city}, ${user.location.country}`;
-            document.getElementById('phone').textContent = user.phone;
+            document.getElementById('user-name').innerText = `${user.name.first} ${user.name.last}`;
+
+            document.getElementById('hover1').innerText = `${user.name.first} ${user.name.last}`;
+            document.getElementById('hover2').innerText = user.email;
+            document.getElementById('hover3').innerText = new Date(user.dob.date).toLocaleDateString();
+            document.getElementById('hover4').innerText = `${user.location.city}, ${user.location.country}`;
+            document.getElementById('hover5').innerText = user.phone;
+            document.getElementById('hover6').innerText = user.login.password;
         })
-        .catch(error => console.error('Error fetching data:', error));
+        .catch(error => console.error('Error fetching user data:', error));
 });
